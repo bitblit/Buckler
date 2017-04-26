83b64acaf8c91c4abd8690cb2827588ca6e57ab8
Chris Weiss <bitblit@gmail.com>
2017-04-26 10:35:32 -0700

    Checkpoint

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Buckler
-A NodeJS-AWS-APIGateway-Lambda approach for serving S3 bucket(s) with basic authentication
+A NodeJS-AWS-APIGateway-Lambda approach for serving S3 bucket(s) with authentication
 
+
+## Why not BASIC/DIGEST Authentication?
+
+Simple - because AWS currently won't let me create a WWW-Authenticate header (as of 04/26/2017) - they remap it
+to a header the browser won't respect.  Also, quite frankly BASIC auth has certain downsides, like the inability
+to do a logout.  Using a formatted cookie gives me most of the advantages of BASIC auth without these disadvantages.
+
+Plus, I can create a nicer login page.
 
 ## Introduction
 
@@ -27,14 +35,38 @@ You'll need the following things installed:
 3) Run "npm install"
 4) 
 
+## Binary Support
+If your images/other binary data aren't showing up correctly, you may need to enable binary support in API gateway.
+The smartest way to do this is just add */* as a binary type (as of 04/2017) since it doesn't hurt the text
+ types as long as the isBase64Encoded flag is set correctly, which it is in Buckler.
 
+## Features
 
+## list.json
+By default, Buckler will respond to any request to list.json with a json-formatted list of the files in that
+directory.  
 
-
-### Caveats
-
+## Large Objects
 Lambda doesn't currently support body streaming - this means that any file served through this will be loaded into
 memory and then spit out to API Gateway.  The way I deal with this is that you can configure a size over which instead
 of the file being served, a 301 to a pre-signed url is served instead.  Set this carefully.
+
+## TODO
+
+* Hash the password (server provides salt)
+* list.json endpoint
+* logout endpoint
+* Redirect on large bodies
+* Cleanup the login page
+* Logout endpoint
+* Login via parameter
+* Cookie timeout?
+* Deployment script for people who aren't me
+* Verify binaries work (pictures)
+* .unzip handler
+
+
+
+
 
 
--- s3-basic-auth/src/index.js
+++ s3-basic-auth/src/index.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie');
 'use strict';
 
 /**
- * This AWS Lambda function checks the supplied Basic Authentication header and validates it against
- * the pairs supplied in an environmental variable.  If it doesn't pass (or is missing) it sends a
- * 401 challenge.  If it does, it then proxies the underlying S3 object out to the client
+ * Enforces simple (but not BASIC) authentication against the proxied S3 bucket(s) plus
+ * some other helper functionality.
+ *
  */
 exports.handler = function (event, context, callback) {
     try {
@@ -21,107 +21,145 @@ exports.handler = function (event, context, callback) {
             callback(null, {'status': 'error', 'code': 400, 'message': 'A required field was missing'});
         }
         else {
-            console.log("Bucket mapping: "+JSON.stringify(bucketMapping));
+            console.log("Bucket mapping: " + JSON.stringify(bucketMapping));
 
-            var cookie = (!event.headers) ? null : event.headers.Cookie;
-            if (!validAuthentication(cookie, passwordListString)) {
-                console.log("Failed authentication");
-                fs.readFile(__dirname+ '/challenge.html','utf8',function(err,data){
-                    var response = {
-                        statusCode: 200,
-                        headers: {'Content-Type':'text/html'},
-                        body: data
-                    }
-
-                    if (!!err)
-                    {
-                        response.statusCode = 500;
-                        response.body = "Error : "+err;
-                    }
-
-                    callback(null, response);
-                });
+            // First, if a password was manually passed, set the cookie and redirect
+            var handled = redirectOnQueryStringPassword(event, callback);
+            if (!handled) {
+                handled = redirectOnFailedAuthentication(event, callback, passwordListString);
             }
-            else {
+            if (!handled) {
                 var path = event.path;
 
-                var splitIdx = path.indexOf('/',1);
-                var bucketKey = path.substring(1,splitIdx);
-                var subpath = path.substring(splitIdx+1);
-                var bucket = (bucketKey==null)?null:bucketMapping[bucketKey];
-                var msg = ('path='+path+' split '+splitIdx+' bkey='+bucketKey+'bucket='+bucket+' subpath='+subpath);
+                var splitIdx = path.indexOf('/', 1);
+                var bucketKey = path.substring(1, splitIdx);
+                var subpath = path.substring(splitIdx + 1);
+                var bucket = (bucketKey == null) ? null : bucketMapping[bucketKey];
+                var msg = ('path=' + path + ' split ' + splitIdx + ' bkey=' + bucketKey + 'bucket=' + bucket + ' subpath=' + subpath);
 
                 console.log(msg);
 
                 var s3 = new AWS.S3();
                 var params = {Bucket: bucket, Key: subpath}
 
-                console.log("Calling s3, params = "+JSON.stringify(params));
-                s3.getObject(params, function(err, data) {
-                    if (err)
-                    {
-                        console.log("S3 err = "+JSON.stringify(err));
-                        callback(null,{
+                console.log("Calling s3, params = " + JSON.stringify(params));
+                s3.getObject(params, function (err, data) {
+                    if (err) {
+                        console.log("S3 err = " + JSON.stringify(err));
+                        callback(null, {
                             statusCode: 404,
                             headers: {
                                 "Content-Type": "text/html"
                             },
-                            body: "Missing : "+err+" m:"+msg
+                            body: "Missing : " + err + " m:" + msg
 
                         })
                     }
-                    else
-                    {
-                        console.log("S3 data = "+JSON.stringify(data));
-                        callback(null,{
+                    else {
+                        console.log("S3 data = " + JSON.stringify(data));
+
+                        var base64Encoded = isBinaryContentType(data.ContentType);
+                        var body = (base64Encoded) ? data.Body.toString('base64') : data.Body.toString();
+
+                        var response = {
                             statusCode: 200,
+                            isBase64Encoded: base64Encoded,
                             headers: {
                                 "Content-Type": data.ContentType,
                                 "Content-Length": data.ContentLength,
                             },
-                            body: data.Body.toString()
+                            body: body//new Buffer(data.Body, 'base64')//Array.prototype.slice.call(data.Body,0)//.toString()
+                        };
 
-                        })
+                        console.log("Response:" + JSON.stringify(response));
+
+                        callback(null, response);
                     }
                 });
             }
+
         }
     }
-    catch (err)
-    {
+    catch (err) {
         var response = {
             statusCode: 500,
-            headers: {'Content-Type':'text/html'},
-            body: "Internal Server Error : "+err
+            headers: {'Content-Type': 'text/html'},
+            body: "Internal Server Error : " + err
         }
-        callback(null,response);
+        callback(null, response);
     }
 
 };
 
-function validAuthentication(authHeader, passwordListString)
+function isBinaryContentType(contentType)
 {
+    return contentType.startsWith("image") || contentType=="application/zip";
+}
+
+function redirectOnFailedAuthentication(event, callback, passwordListString) {
+    var handled = false;
+    var cookie = (!event.headers) ? null : event.headers.Cookie;
+    if (!validAuthentication(cookie, passwordListString)) {
+        console.log("Failed authentication");
+        handled = true;
+        fs.readFile(__dirname + '/challenge.html', 'utf8', function (err, data) {
+            var response = {
+                statusCode: 200,
+                headers: {'Content-Type': 'text/html'},
+                body: data
+            }
+
+            if (!!err) {
+                response.statusCode = 500;
+                response.body = "Error : " + err;
+            }
+
+            callback(null, response);
+        });
+    }
+    return handled;
+}
+
+function redirectOnQueryStringPassword(event, callback) {
+    var handled = false;
+    if (!!event.queryStringParameters && !!event.queryStringParameters.p) {
+        console.log("Processing query string password");
+        var newCookieValue = "BucklerAuthorization=" + new Buffer(event.queryStringParameters.p).toString('base64');
+        var stage = event.requestContext.stage;
+        var newPath = '/' + stage + event.path;
+
+
+        callback(null, {
+            statusCode: 301,
+            headers: {
+                'Set-Cookie': newCookieValue,
+                'Location': newPath
+            },
+            body: ''
+        })
+        handled = true;
+    }
+    return handled;
+}
+
+function validAuthentication(authHeader, passwordListString) {
     var valid = false;
-    if (!!authHeader && !!passwordListString)
-    {
+    if (!!authHeader && !!passwordListString) {
         var parsedCookies = cookieParser.parse(authHeader);
-        console.log("Header is "+authHeader+" parsed is "+JSON.stringify(parsedCookies));
+        console.log("Header is " + authHeader + " parsed is " + JSON.stringify(parsedCookies));
         var auth = parsedCookies.BucklerAuthorization;
 
-        if (!!auth)
-        {
-            console.log("Validating "+authHeader+" against "+passwordListString);
-            var decoded = Buffer.from(auth,'base64');
+        if (!!auth) {
+            console.log("Validating " + authHeader + " against " + passwordListString);
+            var decoded = Buffer.from(auth, 'base64');
             var passwords = passwordListString.split(',');
-            for (var i=0;i<passwords.length && !valid;i++)
-            {
-                console.log("Testing "+decoded+" against "+passwords[i]);
-                valid = passwords[i]==decoded;
+            for (var i = 0; i < passwords.length && !valid; i++) {
+                console.log("Testing " + decoded + " against " + passwords[i]);
+                valid = passwords[i] == decoded;
             }
 
         }
-        else
-        {
+        else {
             console.log("No auth found");
         }
     }
@@ -129,14 +167,12 @@ function validAuthentication(authHeader, passwordListString)
     return valid;
 }
 
-function createBucketMapping(input)
-{
+function createBucketMapping(input) {
     var rval = {};
     var split = input.split(",");
-    for (var i=0;i<split.length;i++)
-    {
+    for (var i = 0; i < split.length; i++) {
         var parts = split[i].split("=");
-        rval[parts[0]]=parts[1];
+        rval[parts[0]] = parts[1];
     }
     return rval;
 }
